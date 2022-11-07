fc1072adc23299b012f14158b9bf97bc803b78e5
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-07 13:29:23 -0800

    starter routes exercises 1-3 complete

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
 // SECTION - Requirements
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser');
 
 // SECTION - Middleware
 
-const myLogger = function (req, res, next) {
-    console.log('LOGGED');
-    next()
-}
-
-const requestTime = function (req, res, next) {
-    req.requestTime = Date.now();
-    next()
-}
-
-app.use(myLogger);
-app.use(requestTime);
-app.use(cookieParser());
-
-// ERROR HANLDER
-app.use((err, req, res, next) => {
-    res.status(400).send(err.message)
-})
-
 // SECTION - Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// app.get('/', (req, res) => {
-//     let responseText = 'Hello World!<br>';
-//     responseText += `<small>Requested at: ${req.requestTime}</small>`
-//     res.send(responseText)
-// })
+app.get('/api/burgers', (req, res) => {
+    res.json(burgers);
+});
 
-app.get('/', function (req, res) {
-    console.log('Cookies: ', req.cookies);
-    console.log('Signed Cookies: ', req.signedCookies);
-})
+app.get('/api/tacos', (req, res) => {
+    res.json(tacos);
+});
 
 // SECTION - Server Start
+app.listen(4000);
 
-app.listen(3000);
+// SECTION - Data
+
+const burgers = [
+    'Hamburger',
+    'Cheese Burger',
+    'Vegetable Burger'
+  ];
+  
+  var tacos = [
+    'Soft Taco',
+    'Crunchy Taco',
+    'Super Taco'
+  ];
