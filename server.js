d4774089078cdc4195169db79c305070ff5b68f1
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-07 14:14:39 -0800

    path.join exercise complete

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 // SECTION - Requirements
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // SECTION - Middleware
 
+app.use('/home', (req, res, next) => {
+    console.log("A new request received at " + Date.now());
+    next();  
+ });
+
+ app.use(express.static('public'))
+
 // SECTION - Routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('The root, bb!')
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 });
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('*', (req, res) => {
+    res.send('404! This is an invalid URL.')
+})
 
 app.get('/api/burgers', (req, res) => {
     res.json(burgers);
